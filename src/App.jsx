0e9adecde3976a77bf5936efa5c2b0b2e32987d9
Midhat <mkazmi2@my.bcit.ca>
2025-07-13 11:41:07 -0700

    added nav functionality

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,18 @@ import Page from "./templates/Page";
 import { useState } from "react";
 
 function App() {
+	const [navHidden, setNavHidden] = useState(true);
+
+	const toggleNav = () => {
+		setNavHidden(function (prevState) {
+			if (prevState === true) {
+				return false;
+			} else {
+				return true;
+			}
+		});
+	};
+
 	return (
 		<>
 			<ScrollToTop />
@@ -32,7 +44,8 @@ function App() {
 								<path d="M480-280q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM80-440q-17 0-28.5-11.5T40-480q0-17 11.5-28.5T80-520h80q17 0 28.5 11.5T200-480q0 17-11.5 28.5T160-440H80Zm720 0q-17 0-28.5-11.5T760-480q0-17 11.5-28.5T800-520h80q17 0 28.5 11.5T920-480q0 17-11.5 28.5T880-440h-80ZM480-760q-17 0-28.5-11.5T440-800v-80q0-17 11.5-28.5T480-920q17 0 28.5 11.5T520-880v80q0 17-11.5 28.5T480-760Zm0 720q-17 0-28.5-11.5T440-80v-80q0-17 11.5-28.5T480-200q17 0 28.5 11.5T520-160v80q0 17-11.5 28.5T480-40ZM226-678l-43-42q-12-11-11.5-28t11.5-29q12-12 29-12t28 12l42 43q11 12 11 28t-11 28q-11 12-27.5 11.5T226-678Zm494 495-42-43q-11-12-11-28.5t11-27.5q11-12 27.5-11.5T734-282l43 42q12 11 11.5 28T777-183q-12 12-29 12t-28-12Zm-42-495q-12-11-11.5-27.5T678-734l42-43q11-12 28-11.5t29 11.5q12 12 12 29t-12 28l-43 42q-12 11-28 11t-28-11ZM183-183q-12-12-12-29t12-28l43-42q12-11 28.5-11t27.5 11q12 11 11.5 27.5T282-226l-42 43q-11 12-28 11.5T183-183Z" />
 							</svg>
 						</button>
-						<button id="Nav-Btn" type="button">
+						<button onClick={toggleNav} type="button">
+							{/* {isNavVisible ? "✕" : "☰"} but using <></> and svgs instead */}
 							<svg
 								className="icon"
 								xmlns="http://www.w3.org/2000/svg"
@@ -47,7 +60,7 @@ function App() {
 						</button>
 					</div>
 				</div>
-				<nav id="Nav" className="site-navigation">
+				<nav style={{ display: navHidden ? "none" : "block" }}>
 					<ul>
 						<li>
 							<NavLink to="/" end>
@@ -180,8 +193,8 @@ function App() {
 				</section>
 				<p className="copyright">
 					<small>
-						&copy; 2016&ndash;2025 <a href="/">Midhat Kazmi</a> |
-						All Rights Reserved
+						&copy; 2016&ndash;{new Date().getFullYear()}{" "}
+						<a href="/">Midhat Kazmi</a> | All Rights Reserved
 					</small>
 				</p>
 			</footer>
