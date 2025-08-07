de6f587c91572b7d2a9b93f9e3ba483f6bcb409f
Midhat <mkazmi2@my.bcit.ca>
2025-08-06 21:42:59 -0700

    added tech stack to homepage

--- src/templates/Home.jsx
+++ src/templates/Home.jsx
@@ -36,6 +36,127 @@ const Home = () => {
 								__html: restData.content.rendered,
 							}}
 						></div>
+						<section className="stack">
+							<figure>
+								<img
+									src="./../src/assets/img/logos/git-logo.svg"
+									alt="Git Logo"
+								/>
+								<figcaption>Git</figcaption>
+							</figure>
+							<figure>
+								<img
+									src="./../src/assets/img/logos/html-logo.svg"
+									alt="Hypertext Markup Language Logo"
+								/>
+								<figcaption>
+									<abbr
+										tabIndex="0"
+										title="Hypertext Markup Language"
+									>
+										HTML5
+									</abbr>
+								</figcaption>
+							</figure>
+							<figure>
+								<img
+									src="./../src/assets/img/logos/css-logo.svg"
+									alt="Cascading Style Sheets Logo"
+								/>
+								<figcaption>
+									<abbr
+										tabIndex="0"
+										title="Cascading Styles Sheets"
+									>
+										CSS3
+									</abbr>
+								</figcaption>
+							</figure>
+							<figure>
+								<img
+									src="./../src/assets/img/logos/sass-logo.svg"
+									alt="Syntactically Awesome Style Sheets Logo"
+								/>
+								<figcaption>
+									<abbr
+										tabIndex="0"
+										title="Syn&shy;tactically Awesome Styles Sheets"
+									>
+										SASS/SCSS
+									</abbr>
+								</figcaption>
+							</figure>
+							<figure>
+								<img
+									src="./../src/assets/img/logos/python-logo.svg"
+									alt="Python Logo"
+								/>
+								<figcaption>Python</figcaption>
+							</figure>
+							<figure>
+								<img
+									src="./../src/assets/img/logos/javascript-logo.svg"
+									alt="JavaScript Logo"
+								/>
+								<figcaption>JavaScript</figcaption>
+							</figure>
+							<figure>
+								<img
+									src="./../src/assets/img/logos/react-logo.svg"
+									alt="React Logo"
+								/>
+								<figcaption>React</figcaption>
+							</figure>
+							<figure>
+								<img
+									src="./../src/assets/img/logos/php-logo.svg"
+									alt="Hypertext Preprocessor Logo"
+								/>
+								<figcaption>
+									<abbr
+										tabIndex="0"
+										title="Hypertext Pre&shy;processor"
+									>
+										PHP
+									</abbr>
+								</figcaption>
+							</figure>
+							<figure>
+								<img
+									src="./../src/assets/img/logos/mysql-logo.svg"
+									alt="My Sequal Query Language Logo"
+								/>
+								<figcaption>
+									<abbr
+										tabIndex="0"
+										title="My Sequel Query Language"
+									>
+										MySQL
+									</abbr>
+								</figcaption>
+							</figure>
+							<figure>
+								<img
+									src="./../src/assets/img/logos/wordpress-logo.svg"
+									alt="WordPress Logo"
+								/>
+								<figcaption>WordPress</figcaption>
+							</figure>
+							<figure>
+								<img
+									src="./../src/assets/img/logos/woocommerce-logo.svg"
+									alt="WooCommerce Logo"
+								/>
+								<figcaption>Woo&shy;Commerce</figcaption>
+							</figure>
+							<figure>
+								<img
+									src="./../src/assets/img/logos/shopify-logo.svg"
+									alt="Shopify Logo"
+								/>
+								<figcaption>Liquid</figcaption>
+							</figure>
+						</section>
 						<Posts />
 						{/* <section id="Work">
 							<h2>Featured Work</h2>
